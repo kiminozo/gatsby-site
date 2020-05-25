3f35d3de80e18a5362c60e1c1519ecc84531ec6a
kiminozo <kiminozoeien@gmail.com>
2020-05-25 19:31:55 +0800

    sass

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import "./header.css"
+import "./header.sass"
 
 const Header = ({ siteTitle }) => (
   <header>

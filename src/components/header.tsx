c05483e154f7a70234e78eec57bb87a9f41d20b7
kiminozo <kiminozoeien@gmail.com>
2020-05-25 12:48:26 +0800

    ts

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,10 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,

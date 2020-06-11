7fb7ca391affbe81b72645a2e7bf3cf962f2d2be
kiminozo <kiminozoeien@gmail.com>
2020-06-11 23:51:48 +0800

    card

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export {
+    default as Image
+} from './image'
+
+export {
+    default as SEO
+} from './seo'
+
+export {
+    default as Header
+} from './header'
+
+export {
+    default as Layout
+} from './header'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,39 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Button } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout";
+
+import logo from "../images/avatar/ritz.jpg"
+
+const CardExampleCard = () => (
+  <Card raised style={{ width: 200, height: 540 }}>
+    <Image src={logo} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card >
+)
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <CardExampleCard />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Button as={Link} to="/demo/page-2/">
       Go to page 2
     </Button>

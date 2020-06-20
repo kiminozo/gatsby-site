6f66a3be174aeab5de58bb6e7646fe69cc00b3bf
kiminozo <kiminozoeien@gmail.com>
2020-06-20 23:53:37 +0800

    delete

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import Layout from "./Layout"
-import TagsLine from "./TagsLine"
-import CC from "./CC"
-import SEO from "./SEO"
-
-//export default Layout
-export {
-    Layout, TagsLine, CC, SEO
-}

ca99b9c3f722bfe534d725685f962d3675d7ddd4
kiminozo <kiminozoeien@gmail.com>
2020-06-24 18:17:32 +0800

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `forritz`,
+        short_name: `forritz`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#fc81aa`,
         display: `minimal-ui`,
         icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
       },

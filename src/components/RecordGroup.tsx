8ee2e90e9dafa9c6c1b78c24a1b2307841d6e246
kiminozo <kiminozoeien@gmail.com>
2020-07-04 18:21:50 +0800

    RecordGroup

--- src/components/RecordGroup.tsx
+++ src/components/RecordGroup.tsx
@@ -1,41 +1,34 @@
-import React, { Component } from "react"
-import { graphql, useStaticQuery, Link } from "gatsby"
+import React from "react"
+import { Link } from "gatsby"
 
 
-import {
-    Icon, Grid, Header, Container, Segment, Divider, Responsive,
-    Button, Card, Image, Label, Item, List
-} from 'semantic-ui-react'
+import { Card, Label } from 'semantic-ui-react'
 import _ from "lodash";
 import { useRecordsData } from "../hooks/useRecordsData"
 import CoverImage from './CoverImage'
 
 
-import demo from "../images/demo.png"
 
 type Props = {
     discographyId: string[];
 }
 
+const lableStyle = { backgroundColor: '#0003', color: '#fff' }
 
 const RecordGroup = (props: Props) => {
     const { discographyId } = props;
 
     const records = useRecordsData();
 
-    //return <div></div>
     const list = records.filter(p => discographyId.indexOf(p.id) >= 0);
     return (
-        <Card.Group doubling centered>
+        <Card.Group stackable centered >
             {list.map(item => (
                 <Card fluid color='teal' as={Link} key={item.id} to={item.slug}>
-                    {/* <CoverImage key={item.id} coverImage={item.coverImage} />
-                    {/* <Label attached='bottom left' color='black'>{item.title}</Label> */}
                     <CoverImage key={item.id} coverImage={item.coverImage} />
-                    {/* <Card.Content>
-                        <Card.Meta>{item.title}</Card.v>
-                    </Card.Content> */}
+                    <Label size="tiny" style={lableStyle} attached='bottom'>{item.title}</Label>
                 </Card>
+
             ))
             }
         </Card.Group >)

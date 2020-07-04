df0ca5e56aa72ddac836b53fc7f106f201a9fb03
kiminozo <kiminozoeien@gmail.com>
2020-07-04 20:54:12 +0800

    DiscographyPage

--- src/pages/discography.tsx
+++ src/pages/discography.tsx
@@ -1,51 +1,67 @@
 import React from "react"
 // Utilities
-import kebabCase from "lodash/kebabCase"
+import _ from "lodash"
 // Components
 import { Link, graphql, PageProps } from "gatsby"
-import { SEO, Layout, CoverImage } from "../components";
+import { SEO, Layout, CoverImage, SideBar } from "../components";
 import {
-    Card
+    Card, Divider, Grid
 } from 'semantic-ui-react'
-import { string } from "prop-types";
 
+interface RecordInfo {
+    coverImage: string;
+    id: string;
+    title: string;
+    slug: string;
+    artist: string
+    categories: string[]
+}
 
 interface Props extends PageProps {
     data: {
         records: {
             nodes: {
-                frontmatter: {
-                    coverImage: string;
-                    id: string;
-                    title: string;
-                    slug: string;
-                }
+                frontmatter: RecordInfo
             }[],
         }
     }
 }
 
+//const cardSize = { width: 150, height: 150 };
+
 
 const DiscographyPage = (props: Props) => {
     const { data: { records: { nodes } } } = props;
     const records = nodes.map(p => p.frontmatter);
-    const cardSize = { width: 150, height: 150 };
+
+    const group = _.groupBy(records, p => p.categories[0]);
+    const categories = _.map(group, (value, key) => ({ category: key, records: value }));
     return (
         <Layout path={props.location.pathname}>
             <SEO title="唱片集" />
-            <div>
-                <h1>唱片集</h1>
-                <Card.Group itemsPerRow={6} doubling>
-                    {records.map(item =>
-                        (
-                            <Card raised style={cardSize} as={Link} key={item.id} to={item.slug}>
-                                <CoverImage key={item.id} coverImage={item.coverImage} />
-                                {/* <Label attached='bottom left'>{item.title}</Label> */}
-                            </Card>
-                        )
-                    )}
-                </Card.Group>
-            </div>
+            <Grid>
+                <Grid.Column mobile={16} computer={13} tablet={14}>
+                    <h1>唱片集</h1>
+                    <Divider />
+                    {
+                        categories.map(({ category, records }) => (
+                            <>
+                                <h2>{category}</h2>
+                                <Card.Group itemsPerRow={5} doubling>
+                                    {records.map(item =>
+                                        (
+                                            <Card as={Link} key={item.id} to={item.slug}>
+                                                <CoverImage key={item.id} coverImage={item.coverImage} />
+                                                {/* <Label attached='bottom left'>{item.title}</Label> */}
+                                            </Card>
+                                        )
+                                    )}
+                                </Card.Group>
+                            </>
+                        ))
+                    }
+                </Grid.Column>
+            </Grid>
         </Layout>
     )
 }
@@ -60,6 +76,8 @@ export const query = graphql`
           id
           title
           slug
+          artist
+          categories
         }
       }
     }

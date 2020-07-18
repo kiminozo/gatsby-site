ab918c67924b9252bc0ae07f5bc8df2daa2ca727
kiminozo <kiminozo.eien@Gmail.com>
2020-07-18 12:21:14 +0800

    preload

--- src/html.js
+++ src/html.js
@@ -11,11 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-
-        {
-          //<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
-        }
+        <link rel="preload" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
